9842c957d0d936ad637218ffaf5960f74b6341f3
Manuel Avello <manuelavello@Manuels-MacBook-Pro.local>
2018-03-31 15:13:19 +0200

    el pedido se realiza por completo

--- src/pages/confirmation/confirmation.ts
+++ src/pages/confirmation/confirmation.ts
@@ -9,16 +9,16 @@ import { OrderProvider } from "../../providers/order/order";
   templateUrl: 'confirmation.html',
 })
 export class ConfirmationPage {
-
+  order: any
   constructor(public navCtrl: NavController, public navParams: NavParams, public orderServ: OrderProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ConfirmationPage');
-    console.log(this.navParams.data)
     const orderId = this.navParams.data
-    this.orderServ.findMyOrder(orderId).subscribe((a)=>
-  console.log(a))
+    this.orderServ.findMyOrder(orderId).subscribe((a)=>{
+
+    this.order = a
+  console.log(this.order)})
 
   }
 
